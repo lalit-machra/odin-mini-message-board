a9001e06fa60241a3292083f49e24f5c2058d182
lalit-machra <lalitmachra0403@gmail.com>
2025-08-29 20:47:32 +0530

    Design home page to display messages

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require("express");
+import express from "express";
+import { indexRouter } from "./routes/index.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.listen(8080, (req, res) => {
+app.use("/", indexRouter);
+
+app.listen(8080, (err) => {
+  if (err) {
+    throw err;
+  }
   console.log("server is running on http://localhost:8080");
 });
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const indexRouter = Router();
+
+let messages = [
+  {
+    text: "hello",
+    user: "amando",
+    added: new Date(),
+    id: 1,
+  },
+  {
+    text: "good morning",
+    user: "charles",
+    added: new Date(),
+    id: 2,
+  }
+]
+
+indexRouter.get("/", (req, res) => {
+  console.log(messages);
+  res.render("index", { messages: messages });
+});
+
+export { indexRouter, messages };
