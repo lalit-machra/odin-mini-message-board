aacf84fc12b4f50c35bcc6c0892ff46e2185c2ea
lalit-machra <lalitmachra0403@gmail.com>
2025-08-29 20:52:31 +0530

    Implement form for new message & display new message on homepage

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { indexRouter } from "./routes/index.js";
+import { newRouter as newMessageRouter } from "./routes/newMessage.js";
 
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+
 app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 
 app.listen(8080, (err) => {
   if (err) {
---
+++ routes/newMessage.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { messages } from "../routes/index.js";
+
+const newRouter = Router();
+
+newRouter.get("/", (req, res) => {
+  console.log("hello")
+  res.render("form");
+});
+
+newRouter.post("/", (req, res) => {
+  const user = req.body.user;
+  const text = req.body.text;
+  const added = new Date();
+  const id = messages.length + 1;
+  messages.push({ user, text, added, id });
+  res.redirect("/");
+});
+
+export { newRouter };
